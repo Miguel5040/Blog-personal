5ccb28680b8e188dc402a05e22447d230f755241
Miguel5040 <miguelssj5040@gmail.com>
2024-03-31 20:50:00 -0700

    articulo de blog agregado

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import MainPicture from "../components/MainPicture"
 import Title from "../components/Title"
+import Content from '../sub-components/Content'
 
 const Articles = () => {
     return (
         <div className="w-full flex flex-col items-center">
             <MainPicture />
-            <div className="max-w-[1200px] w-[75%]">
+            <div className="max-w-[1200px] w-[70%]">
                 <Title />
+                <Content />
             </div>
         </div>
     )
---
+++ src/sub-components/Content.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Content = () => {
+
+    return (
+        <main className="content mt-10 text-[20px] leading-9 flex flex-col items-center">
+            <h3 className="text-[25px] font-medium mb-6 space-y-0 w-full">Lorem ipsum amet consectetur?</h3>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem suscipit nesciunt velit rem, maiores quam eveniet temporibus magni doloribus, quasi veniam necessitatibus ad at dolorum laudantium. Facilis repellat incidunt ab qui sunt nulla vel animi aliquam. Amet exercitationem maxime error voluptatem maiores eos saepe corrupti distinctio minus. Consequuntur eum libero, dicta praesentium dolorem tempora quisquam at minima modi veritatis possimus, est debitis accusantium rerum. Atque, ad quibusdam! Atque delectus molestiae soluta quae impedit, rem, tempore voluptatum autem veritatis dolor omnis.
+            </p>
+
+            <img className="mb-[3rem]" src="./images/grafica.png" alt="Grafica diagrama" />
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti maiores nisi ipsa porro laboriosam minus! Ipsum tempore cum, aliquam rerum facere neque laboriosam consequatur velit error delectus hic! Deserunt, doloremque eos eaque corrupti quibusdam accusamus voluptatibus fugit veritatis delectus officia, veniam recusandae totam velit sunt enim expedita, at itaque iste quaerat! Et modi totam saepe voluptate provident nihil? Atque ipsa excepturi nostrum eligendi, architecto illo laboriosam et doloremque pariatur alias! Nulla maiores fuga quibusdam non soluta saepe deserunt facilis dolorum id, velit eius culpa dolore. Fuga rem eos inventore quia.</p>
+
+            <h3 className="text-[25px] font-medium mb-6 space-y-0 w-full">Lorem ipsum dolor sit amet</h3>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio neque qui distinctio nam consectetur, saepe recusandae deleniti ipsum ad consequatur maiores debitis error magni nemo reprehenderit quam molestiae quidem libero nobis iure. Expedita magni perspiciatis dignissimos, autem alias vero odit voluptatem minima quo est ipsa iure adipisci nostrum eum harum laborum repellendus. Excepturi dicta corporis, quidem quod expedita nemo, adipisci iste eaque eum odio quam corrupti at doloribus natus minus.
+            </p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, repellendus veritatis distinctio modi quo tenetur dignissimos, alias fuga autem est aliquid illum! Alias error et voluptatum delectus iure voluptates pariatur excepturi molestiae veniam nemo, nulla repellendus non, repellat perferendis sunt optio mollitia placeat ab magnam? Quasi eos possimus, quas facilis earum molestias perspiciatis provident quibusdam recusandae vel illum tempora quaerat dolores quod sapiente sint, reprehenderit eveniet? Provident voluptatibus quidem eius hic obcaecati in! Amet unde eum sit quidem voluptatum doloremque!
+            </p>
+
+            <img className="mb-[3rem]" src="./images/grafica2.png" alt="Grafica diagrama numero 2" />
+
+            <h3 className="text-[25px] font-medium mb-6 space-y-0 w-full">Lorem, ipsum dolor?</h3>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, aliquid saepe. Libero quaerat nesciunt similique at doloremque quisquam adipisci amet tempora minus recusandae corporis a facere voluptates, deleniti beatae et labore debitis non temporibus soluta consequuntur in natus distinctio ducimus!
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti libero rerum aperiam explicabo excepturi facere harum maxime facilis quisquam, inventore praesentium accusantium iste debitis dolorum sequi nisi id! Expedita dolorem blanditiis consectetur ut amet quo architecto nostrum eum officiis magni!
+            </p>
+
+            <img className="mb-[3rem] w-[512px] h-[317px]" src="./images/grafica3.png" alt="imgagen grafica number 3" />
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium repellat amet totam debitis provident? Similique suscipit at, numquam debitis velit accusamus sed sint eos nobis vitae excepturi! Necessitatibus, provident sunt?
+            </p>
+
+        </main>
+    )
+}
+
+export default Content
